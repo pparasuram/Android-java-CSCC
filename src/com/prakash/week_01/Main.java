85316492d2c5ce14916e99ff0369bfa630e3b7ef
prakash <pparasuram@yahoo.com>
2016-09-10 21:38:22 -0400

    prakash

--- src/com/prakash/week_01/Main.java
+++ src/com/prakash/week_01/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String ciyName = "Columbus";
+        String cityName = "Columbus";
         int zipCode = 43215;
         float [] temperatures = {32, 25, 27, 40, 45};
 
         float averageTemp = (temperatures[0] + temperatures[1] + temperatures [2]
                 + temperatures [3] + temperatures [4]) / 5;
-        System.out.println("City: " + ciyName);
+        System.out.println("City: " + cityName);
         System.out.println("Zip code: " + zipCode);
         System.out.println("Average temperature: " + averageTemp);
     }
