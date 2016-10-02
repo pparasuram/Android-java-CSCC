c73d0e119c0b3a5dc34709977247e4c5b3c718d1
pparasuram <pparasuram@yahoo.com>
2016-10-02 12:09:06 -0400

    Week4 Assignment Main java fixed

--- src/com/prakash/assignment1/Main.java
+++ src/com/prakash/assignment1/Main.java
@@ -1,5 +1,8 @@
 package com.prakash.assignment1;
 import java.io.Console;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -11,37 +14,96 @@ public class Main {
 // String name = scanner.nextLine();
 
         public static void main(String[] args) {
+            List<String> tasks = new ArrayList<>();
             // write your code here
-            System.out.println("Hello, World!");
+            System.out.println("Hello, Assignment 1!");
             boolean doneFlag = false;
-            String inString;
-            Scanner scanner = new Scanner(System.in);
-            int keyInput;
+            int integerKeyInput;
             while (! doneFlag)
             {
                 displayMenu();
-                inString = scanner.nextLine();
-                keyInput = Integer.parseInt(inString);
-                if (keyInput == 0) {
+                integerKeyInput = getIntegerKeyInput();
+                if (integerKeyInput == 0) {
                     doneFlag = true;
                     break;
                 }
-                else
-                    System.out.println("You entered:" + keyInput);
+                else {
+                    // System.out.println("You entered:" + integerKeyInput);
+                    switch (integerKeyInput) {
+                        case 1: tasks = addATask(tasks);
+                                break;
+                        case 2: tasks = removeATask(tasks);
+                                break;
+                        case 3: tasks = updateATask(tasks);
+                                break;
+                        case 4: listAllTasks(tasks);
+                                break;
+                    } // end of switch
+                } // end of else
 
             } // end of while (! doneFlag)
-            System.out.println("Bye Bye, World!");
-
+            System.out.println("Bye Bye, Assignment 1!");
         }  // end of main ()
-        public static void displayMenu() {
-            // body
-            System.out.println("Please choose an option:");
-            System.out.println("(1) Add a task.");
-            System.out.println("(2) Remove a task.");
-            System.out.println("(3) Update a task.");
-            System.out.println("(4) List all tasks.");
-            System.out.println("(0) Exit.");
+        public static List<String> addATask(List<String> tasks){
+            // int taskNumber;
+            String task;
+            // System.out.println("Enter task number: ");
+            // taskNumber = getIntegerKeyInput();
+            System.out.println("Enter a description of the new task.");
+            task = getStringKeyInput();
+            tasks.add(task);
+            return (tasks);
+        } // public static List<String> addATask(List<String> tasks)
+        public static List<String> removeATask(List<String> tasks) {
+            int taskNumber;
+            String task;
+            System.out.println("Enter the index of the task to remove.");
+            taskNumber = getIntegerKeyInput();
+            tasks.remove(taskNumber);
+            return (tasks);
+        } //        public static List<String> removeATask(List<String> tasks) {
+        public static List<String> updateATask(List<String> tasks){
+            int taskNumber;
+            String task;
+            System.out.println("Enter the index of the task to update.");
+            taskNumber = getIntegerKeyInput();
+            System.out.println("Enter the new description of the task.");
+            task = getStringKeyInput();
+            tasks.remove (taskNumber);
+            tasks.add(taskNumber, task);
+            return (tasks);
+        } // public static List<String> updateATask(List<String> tasks)
+        public static void listAllTasks(List<String> tasks){
+            int taskNumber;
+            String task;
+            int last;
+            last = tasks.size();
+            for (int i=0; i < last; i++)
+            {
+                System.out.println(i + ". " + tasks.get(i));
+            }
+        } // public static void listAllTasks(List<String> tasks){
+        public static int getIntegerKeyInput(){
+                String inString;
+                Scanner scanner = new Scanner(System.in);
+                inString = scanner.nextLine();
+                return (Integer.parseInt(inString));
+            }
+        public static String getStringKeyInput(){
+            String inString;
+            Scanner scanner = new Scanner(System.in);
+            inString = scanner.nextLine();
+            return (inString);
         }
+        public static void displayMenu() {
+                // body
+                System.out.println("Please choose an option:");
+                System.out.println("(1) Add a task.");
+                System.out.println("(2) Remove a task.");
+                System.out.println("(3) Update a task.");
+                System.out.println("(4) List all tasks.");
+                System.out.println("(0) Exit.");
+            } // public static void displayMenu()
 
 } // end of main
 
